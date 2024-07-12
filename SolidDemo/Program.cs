2de0ed9dce16e41db6b32a7a810789badd127a59
Bryan Joseph Aguinaldo <bryanjoseph.aguinaldo@infor.com>
2024-07-12 11:57:19 +0800

    Add project files.

---
+++ SolidDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SolidDemo.Accounts;
+using SolidDemo.Validations;
+
+namespace SolidDemo;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var serviceCollection = new ServiceCollection();
+
+        serviceCollection.AddScoped<ILoggingService, LoggingService>();
+        serviceCollection.AddScoped<IBankService, BankService>();  
+        serviceCollection.AddScoped<IAccountValidation, SavingsAccountValidation>();
+        serviceCollection.AddScoped<IAccountValidation, CurrentAccountValidation>();
+        serviceCollection.AddScoped<IAccountValidation, TimeDepositValidation>();
+
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+
+        var bankService = serviceProvider.GetRequiredService<IBankService>();
+
+        var savingsAccount = new SavingsAccount(1001, 500.00m);
+        var currentAccount = new CurrentAccount(1002, 500.00m, 100m);
+        var timeDepositAccount = new TimeDepositAccount(1003, 500m, DateTime.Today.Subtract(TimeSpan.FromDays(29)), 30);
+
+        var customer = new Customer(1, "Juan Dela Cruz", new List<IAccount>
+        {
+            savingsAccount,
+            currentAccount,
+            timeDepositAccount,
+        });
+
+
+        bankService.Withdraw(customer, 1001, 100.00m);
+        bankService.Withdraw(customer, 1002, 600.00m);
+        bankService.Withdraw(customer, 1003, 300m);
+
+        Console.ReadLine();
+    }
+}
